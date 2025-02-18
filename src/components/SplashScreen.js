188c5d8cb25f8f9fdffe2f03d135fdd2c22be269
Disitha <disithar@gmail.com>
2025-02-18 12:38:40 +0530

    Add SplashScreen component for initial loading state

---
+++ src/components/SplashScreen.js
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+import React from 'react';
+import {View, Text} from 'react-native';
+export default function SplashScreen() {
+  return (
+    <div className="splash-screen">
+      <h1>Loading...</h1>
+    </div>
+  );
+}
