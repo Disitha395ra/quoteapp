e0225643ee25a981f00143a8acb2b7b4219b6fa0
Disitha <disithar@gmail.com>
2025-02-18 12:07:56 +0530

    Add Firebase configuration files and initial security rules

---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,9 @@
+const { , validateArgs } = require('firebase/data-connect');
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'quoteapp',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
+
